5ed6542222635ef5a242b21dd9143c9de76c9d12
A. Roberto Fischer <a.robertofischer@gmail.com>
2017-04-26 14:27:59 +0200

    s

--- src/HackerRank/NSECodesprint/CrossOver.scala
+++ src/HackerRank/NSECodesprint/CrossOver.scala
@@ -4,7 +4,6 @@ import java.io.{ByteArrayInputStream, IOException, PrintWriter}
 import java.util.InputMismatchException
 
 import scala.collection.generic.CanBuildFrom
-import scala.collection.mutable
 import scala.language.higherKinds
 
 /**
@@ -13,13 +12,66 @@ import scala.language.higherKinds
   * @author A. Roberto Fischer <a.robertofischer@gmail.com> on 4/26/2017
   */
 object CrossOver {
+
   private val INPUT = ""
 
   //------------------------------------------------------------------------------------------//
   // Solution                                                                
   //------------------------------------------------------------------------------------------//
   private def solve(): Unit = {
+    val n = nextInt()
+    val prices = nextInt[Array](n)
 
+    val result = crossOverTriplet(prices)
+
+    if (result.nonEmpty) {
+      out.println(result.map(_.productIterator.mkString(" ")).mkString("\n"))
+    }
+  }
+
+  def shortTermMovingAvg(prices: Array[Int]): Iterator[Double] = {
+    prices.sliding(60).map(_.sum / 60.0)
+  }
+
+  def longTermMovingAvg(prices: Array[Int]): Iterator[Double] = {
+    prices.sliding(300).map(_.sum / 300.0)
+  }
+
+  def combine(shortTermMovingAvg: Iterator[Double], longTermMovingAvg: Iterator[Double]): Iterator[(Int, (Double, Double))] = {
+    shortTermMovingAvg.drop(240).zip(longTermMovingAvg).zipWithIndex.map(pair =>
+      (pair._2 + 300, pair._1))
+  }
+
+  def isCrossOver(previous: (Double, Double), current: (Double, Double)): Boolean = {
+    val (previousShortTerm, previousLongTerm) = previous
+    val (currentShortTerm, currentLongTerm) = current
+    if (previousShortTerm > previousLongTerm && currentShortTerm <= currentLongTerm) {
+      true
+    } else if (previousShortTerm < previousLongTerm && currentShortTerm >= currentLongTerm) {
+      true
+    } else if (previousShortTerm == previousLongTerm && currentShortTerm != currentLongTerm) {
+      true
+    } else {
+      false
+    }
+  }
+
+  def round(input: Double): Double = {
+    Math.round(input * 100.0) / 100.0
+  }
+
+  def crossOver(shortTermMovingAvg: Iterator[Double], longTermMovingAvg: Iterator[Double]): Iterator[(Int, (Double, Double))] = {
+    val combined = combine(shortTermMovingAvg, longTermMovingAvg)
+
+    combined.sliding(2).map(pair => (pair.head, pair.last)).filter {
+      case (previous, current) =>
+        isCrossOver(previous._2, current._2)
+    }.map(_._2)
+  }
+
+  def crossOverTriplet(prices: Array[Int]): Iterator[(Int, String, String)] = {
+    crossOver(shortTermMovingAvg(prices), longTermMovingAvg(prices)).map(pair =>
+      (pair._1, f"${round(pair._2._1)}%1.2f", f"${round(pair._2._2)}%1.2f"))
   }
 
   //------------------------------------------------------------------------------------------//

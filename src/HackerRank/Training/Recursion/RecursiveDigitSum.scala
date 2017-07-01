6ecb06e3ec3179f0bcd76d858113a1a045216d23
A. Roberto Fischer <a.robertofischer@gmail.com>
2017-07-01 14:02:32 +0200

    RecursiveDigitSum

---
+++ src/HackerRank/Training/Recursion/RecursiveDigitSum.scala
@@ -0,0 +1,128 @@
+package HackerRank.Training.Recursion
+
+import java.io.{ByteArrayInputStream, IOException, PrintWriter}
+import java.util.InputMismatchException
+
+import scala.annotation.tailrec
+import scala.language.higherKinds
+
+/**
+  * Copyright (c) 2017 A. Roberto Fischer
+  *
+  * @author A. Roberto Fischer <a.robertofischer@gmail.com> on 6/30/2017
+  */
+private object RecursiveDigitSum {
+  private val INPUT = ""
+
+  //------------------------------------------------------------------------------------------//
+  // Solution                                                                
+  //------------------------------------------------------------------------------------------//
+  private def solve(): Unit = {
+        println(superDigit(nextString(), nextInt()))
+  }
+
+
+  private def superDigit(number: String, k: Int) = {
+
+    @tailrec
+    def _superDigit(number: String): Int = {
+      if (number.length <= 1) {
+        number.head - '0'
+      } else {
+        _superDigit(number.foldLeft(0L)(_ + _ - '0').toString)
+      }
+    }
+
+    _superDigit((number.foldLeft(0L)(_ + _ - '0') * k).toString)
+  }
+
+  //------------------------------------------------------------------------------------------//
+  // Input-Output                                                                 
+  //------------------------------------------------------------------------------------------//
+  private var in: java.io.InputStream = _
+  private var out: java.io.PrintWriter = _
+
+  private def println(x: Any) = out.println(x)
+
+  @throws[Exception]
+  def main(args: Array[String]): Unit = {
+    run()
+  }
+
+  @throws[Exception]
+  private def run(): Unit = {
+    in = if (INPUT.isEmpty) System.in else new ByteArrayInputStream(INPUT.getBytes)
+    out = new PrintWriter(System.out)
+
+    val s = System.currentTimeMillis
+    solve()
+    out.flush()
+    if (!INPUT.isEmpty) System.out.println(System.currentTimeMillis - s + "ms")
+  }
+
+  private def nextString(): String = {
+    var b = skip
+    val sb = new java.lang.StringBuilder
+    while (!isSpaceChar(b)) {
+      sb.appendCodePoint(b)
+      b = readByte()
+    }
+    sb.toString
+  }
+
+  private def nextInt(): Int = {
+    var num = 0
+    var b = 0
+    var minus = false
+    while ( {
+      b = readByte()
+      b != -1 && !((b >= '0' && b <= '9') || b == '-')
+    }) {}
+    if (b == '-') {
+      minus = true
+      b = readByte()
+    }
+    while (true) {
+      if (b >= '0' && b <= '9') {
+        num = num * 10 + (b - '0')
+      } else {
+        if (minus) return -num else return num
+      }
+      b = readByte()
+    }
+    throw new IOException("Read Int")
+  }
+
+  private val inputBuffer = new Array[Byte](1024)
+  private var lenBuffer = 0
+  private var ptrBuffer = 0
+
+  private def readByte(): Int = {
+    if (lenBuffer == -1) throw new InputMismatchException
+    if (ptrBuffer >= lenBuffer) {
+      ptrBuffer = 0
+      try {
+        lenBuffer = in.read(inputBuffer)
+      } catch {
+        case _: IOException =>
+          throw new InputMismatchException
+      }
+      if (lenBuffer <= 0) return -1
+    }
+    inputBuffer({
+      ptrBuffer += 1
+      ptrBuffer - 1
+    })
+  }
+
+  private def isSpaceChar(c: Int) = !(c >= 33 && c <= 126)
+
+  private def skip = {
+    var b = 0
+    while ( {
+      b = readByte()
+      b != -1 && isSpaceChar(b)
+    }) {}
+    b
+  }
+}

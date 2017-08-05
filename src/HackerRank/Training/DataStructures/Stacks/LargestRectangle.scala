2d42946ecd1ebef33e1950d7eb8d9b4f525a43bc
A. Roberto Fischer <a.robertofischer@gmail.com>
2017-08-05 14:54:52 +0200

    LargestRectangle

---
+++ src/HackerRank/Training/DataStructures/Stacks/LargestRectangle.scala
@@ -0,0 +1,270 @@
+package HackerRank.Training.DataStructures.Stacks
+
+import java.io.{ByteArrayInputStream, IOException, PrintWriter}
+import java.util.InputMismatchException
+
+import scala.annotation.tailrec
+import scala.collection.generic.CanBuildFrom
+import scala.collection.mutable
+import scala.language.higherKinds
+import scala.reflect.ClassTag
+
+/**
+  * Copyright (c) 2017 A. Roberto Fischer
+  *
+  * @author A. Roberto Fischer <a.robertofischer@gmail.com> on 8/5/2017
+  */
+private[this] object LargestRectangle {
+
+  import Reader._
+  import Writer._
+
+  private[this] val TEST_INPUT: Option[String] = None
+
+  //------------------------------------------------------------------------------------------//
+  // Solution
+  //------------------------------------------------------------------------------------------//
+  private[this] def solve(): Unit = {
+    val n = nextInt()
+    println(Histogram(next[Long, Vector](n)).maxArea)
+  }
+
+  private[this] class Histogram[@specialized(Int, Long, Double) T: Numeric](underlying: Seq[(T, T)]) {
+    def maxArea: T = {
+      import Numeric.Implicits._
+      import Ordering.Implicits._
+
+      val histogramWithWidthOne = underlying.map { case (height, width) => height * width }
+      val stack = new mutable.ArrayStack[Int]()
+      var maxArea = implicitly[Numeric[T]].zero
+
+      def area(right: Int, stack: mutable.ArrayStack[Int] = stack) = {
+        val left = stack.pop()
+        val smallestBar = histogramWithWidthOne(left)
+        val boxWidth = if (stack.isEmpty) right else right - stack.head - 1
+        val area = smallestBar * implicitly[Numeric[T]].fromInt(boxWidth)
+
+        if (maxArea < area) {
+          maxArea = area
+        }
+      }
+
+      @tailrec
+      def iterate(i: Int = 0, j: Int = 0, stack: mutable.ArrayStack[Int] = stack): Int = {
+        if (i == histogramWithWidthOne.size) {
+          i
+        } else {
+          if (stack.isEmpty || histogramWithWidthOne(stack.head) <= histogramWithWidthOne(i)) {
+            stack.push(i)
+            iterate(i + 1, j + 1)
+          } else {
+            area(i, stack)
+            iterate(i, j + 1)
+          }
+        }
+      }
+
+      val i = iterate()
+
+      while (stack.nonEmpty) {
+        area(i)
+      }
+
+      maxArea
+    }
+  }
+
+  private[this] object Histogram {
+    def apply[T: Numeric](underlying: Seq[T]): Histogram[T] = {
+      new Histogram(underlying.zip(Stream.continually(implicitly[Numeric[T]].one)))
+    }
+  }
+
+  //------------------------------------------------------------------------------------------//
+  // Run
+  //------------------------------------------------------------------------------------------//
+  @throws[Exception]
+  def main(args: Array[String]): Unit = {
+    val s = System.currentTimeMillis
+    solve()
+    flush()
+    if (TEST_INPUT.isDefined) System.out.println(System.currentTimeMillis - s + "ms")
+  }
+
+  //------------------------------------------------------------------------------------------//
+  // Input
+  //------------------------------------------------------------------------------------------//
+  private[this] final object Reader {
+
+    private[this] implicit val in = TEST_INPUT.fold(System.in)(s => new ByteArrayInputStream(s.getBytes))
+
+    def next[T, Coll[_]](reader: () => T, n: Int)
+                        (implicit cbf: CanBuildFrom[Coll[T], T, Coll[T]]): Coll[T] = {
+      val builder = cbf()
+      builder.sizeHint(n)
+      for (_ <- 0 until n) {
+        builder += reader()
+      }
+      builder.result()
+    }
+
+    def nextWithIndex[T, Coll[_]](reader: () => T, n: Int)
+                                 (implicit cbf: CanBuildFrom[Coll[(T, Int)], (T, Int), Coll[(T, Int)]]): Coll[(T, Int)] = {
+      val builder = cbf()
+      builder.sizeHint(n)
+      for (i <- 0 until n) {
+        builder += ((reader(), i))
+      }
+      builder.result()
+    }
+
+    def next[T: ClassTag, Coll[_]](n: Int)
+                                  (implicit cbf: CanBuildFrom[Coll[T], T, Coll[T]]): Coll[T] = {
+      val builder = cbf()
+      builder.sizeHint(n)
+      for (_ <- 0 until n) {
+        builder += next[T]()
+      }
+      builder.result()
+    }
+
+    def nextWithIndex[T: ClassTag, Coll[_]](n: Int)
+                                           (implicit cbf: CanBuildFrom[Coll[(T, Int)], (T, Int), Coll[(T, Int)]]): Coll[(T, Int)] = {
+      val builder = cbf()
+      builder.sizeHint(n)
+      for (i <- 0 until n) {
+        builder += ((next[T](), i))
+      }
+      builder.result()
+    }
+
+    def nextMultiLine[T: ClassTag](n: Int, m: Int): Seq[Seq[T]] = {
+      val map = Vector.newBuilder[Vector[T]]
+      var i = 0
+      while (i < n) {
+        map += next[T, Vector](m)
+        i += 1
+      }
+      map.result()
+    }
+
+    def nextDouble(): Double = nextString().toDouble
+
+    def nextChar(): Char = skip.toChar
+
+    def nextString(): String = {
+      var b = skip
+      val sb = new java.lang.StringBuilder
+      while (!isSpaceChar(b)) {
+        sb.appendCodePoint(b)
+        b = readByte()
+      }
+      sb.toString
+    }
+
+    def nextInt(): Int = {
+      var num = 0
+      var b = 0
+      var minus = false
+      while ( {
+        b = readByte()
+        b != -1 && !((b >= '0' && b <= '9') || b == '-')
+      }) {}
+      if (b == '-') {
+        minus = true
+        b = readByte()
+      }
+      while (true) {
+        if (b >= '0' && b <= '9') {
+          num = num * 10 + (b - '0')
+        } else {
+          if (minus) return -num else return num
+        }
+        b = readByte()
+      }
+      throw new IOException("Read Int")
+    }
+
+    def nextLong(): Long = {
+      var num = 0L
+      var b = 0
+      var minus = false
+      while ( {
+        b = readByte()
+        b != -1 && !((b >= '0' && b <= '9') || b == '-')
+      }) {}
+      if (b == '-') {
+        minus = true
+        b = readByte()
+      }
+      while (true) {
+        if (b >= '0' && b <= '9') {
+          num = num * 10 + (b - '0')
+        } else {
+          if (minus) return -num else return num
+        }
+        b = readByte()
+      }
+      throw new IOException("Read Long")
+    }
+
+    private[this] def next[T: ClassTag](): T = {
+      implicitly[ClassTag[T]].runtimeClass match {
+        case java.lang.Integer.TYPE => nextInt().asInstanceOf[T]
+        case java.lang.Long.TYPE => nextLong().asInstanceOf[T]
+        case java.lang.Double.TYPE => nextDouble().asInstanceOf[T]
+        case java.lang.Character.TYPE => nextChar().asInstanceOf[T]
+        case s if Class.forName("java.lang.String") == s => nextString().asInstanceOf[T]
+        case _ => throw new RuntimeException("Unsupported input type.")
+      }
+    }
+
+    private[this] val inputBuffer = new Array[Byte](1024)
+    private[this] var lenBuffer = 0
+    private[this] var ptrBuffer = 0
+
+    private[this] def readByte()(implicit in: java.io.InputStream): Int = {
+      if (lenBuffer == -1) throw new InputMismatchException
+      if (ptrBuffer >= lenBuffer) {
+        ptrBuffer = 0
+        try {
+          lenBuffer = in.read(inputBuffer)
+        } catch {
+          case _: IOException =>
+            throw new InputMismatchException
+        }
+        if (lenBuffer <= 0) return -1
+      }
+      inputBuffer({
+        ptrBuffer += 1
+        ptrBuffer - 1
+      })
+    }
+
+    private[this] def isSpaceChar(c: Int) = !(c >= 33 && c <= 126)
+
+    private[this] def skip = {
+      var b = 0
+      while ( {
+        b = readByte()
+        b != -1 && isSpaceChar(b)
+      }) {}
+      b
+    }
+  }
+
+  //------------------------------------------------------------------------------------------//
+  // Output
+  //------------------------------------------------------------------------------------------//
+  private[this] final object Writer {
+
+    private[this] val out = new PrintWriter(System.out)
+
+    def flush(): Unit = out.flush()
+
+    def println(x: Any): Unit = out.println(x)
+
+    def print(x: Any): Unit = out.print(x)
+  }
+
+}
